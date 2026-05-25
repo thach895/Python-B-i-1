507e7e5b98059a8c18c2c92031c5fdeff8675c3b
thach895 <ngo67775@gmail.com>
2026-05-25 16:17:46 +0700

    Add files via upload

---
+++ Bài 1.py
@@ -0,0 +1,10 @@
+print(' --- HỆ THỐNG TIEP NHẬN BỆNH NHÂN --- ')
+
+name_patient = input('Nhập tên bệnh nhân: ')
+age = int(input('Mời bạn nhập tuổi: '))
+symptom = input('Mời bạn nhập triệu chứng bệnh: ')
+
+print(' --- PHIẾU KHÁM BỆNH --- ')
+print('Tên bệnh nhân:', name_patient)
+print('Tuổi:', age)
+print('Triệu chứng:', symptom)
