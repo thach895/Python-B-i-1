578d0608d4289c33437336644a7e52ecb0042b6d
thach895 <ngo67775@gmail.com>
2026-06-03 20:52:30 +0700

    Add files via upload

---
+++ Bài 1 S11 PY.py
@@ -0,0 +1,19 @@
+product_info = ("SP001", "Áo polo nam", "Size L", 299000)
+
+product_code = product_info[0]
+
+product_name = product_info[1]
+
+product_length = len(product_info)
+
+product_info = (
+    product_info[0],
+    product_info[1],
+    product_info[2],
+    279000
+)
+
+print("Mã sản phẩm:", product_code)
+print("Tên sản phẩm:", product_name)
+print("Số lượng thông tin sản phẩm:", product_length)
+print("Thông tin sản phẩm sau cập nhật:", product_info)
